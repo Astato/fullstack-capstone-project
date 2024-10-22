9a2077be0b3442a703544242181874fe1077e0cc
Astato <leivagfernando@outlook.com>
2024-10-22 03:18:38 -0400

    Set gift route

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
 // MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
+const url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
 const dbName = "giftdb";
@@ -14,15 +14,14 @@ async function connectToDatabase() {
     };
 
     const client = new MongoClient(url);      
-
+    
     // Task 1: Connect to MongoDB
-    // {{insert code}}
-
+    await client.connect()
     // Task 2: Connect to database giftDB and store in variable dbInstance
     //{{insert code}}
-
+    dbInstance = client.db("giftDB")
     // Task 3: Return database instance
-    // {{insert code}}
+    return dbInstance
 }
 
 module.exports = connectToDatabase;
