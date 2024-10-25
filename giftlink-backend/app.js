39d07fa499044419639e6a7533ba1cb6bde48bc3
Astato <97934945+Astato@users.noreply.github.com>
2024-10-25 04:08:28 -0300

    Add missing semicolons app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -24,9 +24,9 @@ app.use(express.json());
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 //{{insert code here}}
-const giftRoutes = require("./routes/giftRoutes")
+const giftRoutes = require("./routes/giftRoutes");
 const searchRoutes = require('./routes/searchRoutes');
-const authRoutes = require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes");
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
@@ -39,9 +39,9 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use("/api/gifts", giftRoutes)
+app.use("/api/gifts", giftRoutes);
 app.use('/api/search', searchRoutes);
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
 
@@ -53,8 +53,8 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
