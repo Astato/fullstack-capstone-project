6d68aaacf2b2e784c6537956d3852b39a0a80bd3
Astato <97934945+Astato@users.noreply.github.com>
2024-10-25 04:16:55 -0300

    Update giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,7 +1,8 @@
 /*jshint esversion: 8 */
-const DBConnection = require("../models/db")
-const express = require("express")
-const router = express.Router()
+const DBConnection = require("../models/db");
+const express = require("express");
+const router = express.Router();
+
 router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
